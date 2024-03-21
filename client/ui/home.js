d8774909f2647c883e0562ee2669bd4db46ed7b4
Denis Perov <denis.perov@gmail.com>
2024-03-21 16:07:54 +0300

    [WIP] Support TG20 Deep Links

--- client/ui/home.js
+++ client/ui/home.js
@@ -1,1 +1,37 @@
+import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'miku-web-app';
+import { TemplateController } from 'meteor/space:template-controller';
 import './home.html';
+
+TemplateController('Home', {
+  state: {},
+  onCreated() {},
+  onRendered() {},
+  helpers: {},
+  events: {
+    'click [data-action=openLink]'() {
+      WebApp.openLink('https://ya.ru', {
+        try_instant_view: true,
+      });
+    },
+    'click [data-action=requestTransfer]'() {
+      const { wallet, tick } = Meteor.settings.public;
+      const data = {
+        type: 'transfer',
+        to: wallet,
+        amount: 100,
+        tick,
+        memo: '1234',
+      };
+      const encodedData = encodeURIComponent(JSON.stringify(data))
+        .replace(/%/g, '--')
+        .replace(/=/g, '__')
+        .replace(/&/g, '-');
+      WebApp.openTelegramLink(`https://t.me/tg20/app?startapp=${encodedData}`);
+    },
+    'click [data-action=closeApp]'() {
+      WebApp.close();
+    },
+  },
+  private: {},
+});
