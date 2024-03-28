36918a68c60d14eaeca696e72c892aad398c5206
Denis Perov <denis.perov@gmail.com>
2024-03-28 17:45:12 +0300

    Introduce UserAccounts support

--- init.js
+++ init.js
@@ -1,5 +1,6 @@
-import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
+import { Meteor } from 'meteor/meteor';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 SimpleSchema.debug = Meteor.isDevelopment;
 SimpleSchema.extendOptions(['autoform']);
@@ -12,3 +13,62 @@ SimpleSchema.extendOptions(['autoform']);
 //     },
 //   },
 // });
+
+/**
+ * @see https://github.com/meteor-useraccounts/core/blob/master/Guide.md#features
+ */
+AccountsTemplates.configure({
+  // Behavior
+  confirmPassword: false,
+  enablePasswordChange: false,
+  forbidClientAccountCreation: false,
+  overrideLoginErrors: true,
+  sendVerificationEmail: false,
+  lowercaseUsername: false,
+  focusFirstInput: true,
+  // Appearance
+  showAddRemoveServices: false,
+  showForgotPasswordLink: false,
+  showLabels: true,
+  showPlaceholders: true,
+  showResendVerificationEmailLink: false,
+  // Client-side Validation
+  continuousValidation: false,
+  negativeFeedback: false,
+  negativeValidation: true,
+  positiveValidation: true,
+  positiveFeedback: true,
+  showValidating: true,
+  // Privacy Policy and Terms of Use
+  // privacyUrl: 'privacy',
+  // termsUrl: 'terms-of-use',
+  // Redirects
+  homeRoutePath: '/',
+  redirectTimeout: 4000,
+  // Hooks
+  // onLogoutHook: myLogoutFunc,
+  // onSubmitHook: mySubmitFunc,
+  // preSignUpHook: myPreSubmitFunc,
+  // postSignUpHook: myPostSubmitFunc,
+  // Texts
+  // texts: {
+  //   button: {
+  //     signUp: "Register Now!"
+  //   },
+  //   socialSignUp: "Register",
+  //   socialIcons: {
+  //       "meteor-developer": "fa fa-rocket"
+  //   },
+  //   title: {
+  //       forgotPwd: "Recover Your Password"
+  //   },
+  // },
+  /**
+   * @see https://github.com/meteor-compat/useraccounts-flow-routing-extra/?tab=readme-ov-file#blaze-configuration
+   */
+  ...(Meteor.isClient && {
+    defaultLayout: 'Layout',
+    defaultLayoutRegions: {},
+    defaultContentRegion: 'main',
+  }),
+});
