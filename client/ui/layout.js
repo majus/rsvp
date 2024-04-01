10015cc354949935615d90fb371c74ec57e35085
Denis Perov <denis.perov@gmail.com>
2024-04-01 15:44:07 +0300

    Utilize {{navbar}} UI component from imajus:bootstrap-helpers package

--- client/ui/layout.js
+++ client/ui/layout.js
@@ -1,1 +1,30 @@
+import { TemplateController } from 'meteor/space:template-controller';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import './layout.html';
+
+TemplateController('Layout', {
+  helpers: {
+    organiserItems: () => [
+      {
+        label: 'Events',
+        link: FlowRouter.path('Events.browse'),
+        active: () =>
+          ['Events.browse', 'Events.create', 'Events.edit'].includes(
+            FlowRouter.getRouteName(),
+          ),
+      },
+      {
+        label: 'RSVPs',
+        link: FlowRouter.path('Registrations.browse'),
+        active: () => FlowRouter.getRouteName() === 'Registrations.browse',
+      },
+    ],
+    visitorItems: () => [
+      {
+        label: 'Sign in',
+        link: FlowRouter.path('atSignIn'),
+        active: () => FlowRouter.getRouteName() === 'atSignIn',
+      },
+    ],
+  },
+});
